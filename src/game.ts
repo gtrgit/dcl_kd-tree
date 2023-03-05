f256594cdb3e093300e140538c00fa91f2f2171b
Grant Ryan <gtrfrost@gmail.com>
2023-03-05 21:58:10 +1100

    made prettier

--- src/game.ts
+++ src/game.ts
@@ -15,13 +15,6 @@ const ent2 = new Entity();
 ent2.addComponent(resources.models.hexBlue);
 ent2.addComponent(new Transform({ position: new Vector3(4, 1, 2) }));
 tileList.push(ent2);
-ent2.addComponent(
-  new OnPointerDown((e) => {
-    for (let i = 0; i < tileList.length; i++) {
-      // log(tileList[i].getComponent(Transform).position)
-    }
-  })
-);
 
 engine.addEntity(ent2);
 
